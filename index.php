934fd5a7323d8692792b2adf4b700910e9a77dc7
asharfifabrics <69179490+asharfifabrics@users.noreply.github.com>
2021-09-14 17:14:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $cfile = fopen("newfile.txt", "r") or die("Unable to open file!");
 $readurl=fgets($cfile);
 fclose($cfile);
-header("location:".$readul);
+header("location:".$readurl);
 exit();
 ?>
